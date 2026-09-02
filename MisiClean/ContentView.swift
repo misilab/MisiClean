ae4860a9e7b5b134113015b27bf359587fbf65cb
Misiraca <contact@misiraca.com>
2026-09-02 10:32:45 +0200

    Initial Commit

---
+++ MisiClean/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MisiClean
+//
+//  Created by Matthieu Misiraca on 02/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MisiClean/MisiCleanApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisiCleanApp.swift
+//  MisiClean
+//
+//  Created by Matthieu Misiraca on 02/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct MisiCleanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
